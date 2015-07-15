66e7c915e581c0f02b5b598af9881232a72ffe85
faxg <florian.alexander.georg@gmail.com>
2015-07-15 17:06:29 +0200

    fixed node-red entry point

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var connect = require("connect");
 
+// IBM Bluemix Cloud Mobile Services
+var ibmbluemix = require('ibmbluemix');
+var ibmsecurity= = require('ibmsecurity');
+var ibmcloudcode= = require('ibmcloudcode');
+var ibmdata= = require('ibmdata');
+var ibmpush= = require('ibmpush');
+var config = {
+	applicationId:"<ApplicationID>",
+	applicationRoute:"<ApplicationRoute>"
+};
+ibmbluemix.initialize(config);
+
+
+
+
+
 var port = (process.env.VCAP_APP_PORT || 3000);
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 var http = require('http');
+
 
 var app = connect().use(connect.static(__dirname + '/public'))
 
 
 http.createServer(app).listen(port,host);
-
